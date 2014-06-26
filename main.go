3529769a6cd6ffd70802ce76a2e9f95d12fb397e
Jeff Escalante <hello@jenius.me>
2014-06-26 18:27:37 -0400

    implement basic http server

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
+// Package main is the command line interface to the burner api. It can be used
+// to stop and start the server.
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+)
+
+func handler(res http.ResponseWriter, req *http.Request) {
+  fmt.Fprint(res, "hello world!")
+}
 
 func main() {
-  fmt.Println("hello world!")
+	http.HandleFunc("/", handler)
+  http.ListenAndServe(":1111", nil)
 }
