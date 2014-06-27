b3dcf21ff4f38d194b5d031d79d1578d8bde1c9e
Jeff Escalante <hello@jenius.me>
2014-06-27 19:12:08 -0400

    recieves a POSTed file and writes it

--- main.go
+++ main.go
@@ -6,30 +6,50 @@ import (
   "fmt"
   "net/http"
   "bytes"
-  "encoding/json"
+  "os"
+  "log"
+  "path/filepath"
+  "github.com/nu7hatch/gouuid"
 )
 
-func handler(res http.ResponseWriter, req *http.Request) {
-  params := GetParams(req)
-  fmt.Println(params.File)
-  fmt.Fprint(res, "hello world!")
+func hostFile(res http.ResponseWriter, req *http.Request) {
+  // get the contents of the POSTed file
+  contents := GetBody(req)
+
+  // create a new uuid
+  id, err := uuid.NewV4()
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Println(id)
+
+  // create a new file named the uuid .tar.gz in the 'files' directory
+  dirname, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+  if err != nil {
+    log.Fatal(err)
+  }
+  fPath := filepath.Join(dirname, "files", id.String() + ".tar.gz")
+  file, err := os.Create(fPath)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  // write the contents of the POSTed file to it
+  file.Write(contents)
+
+  // return the id as the response
+  fmt.Fprint(res, id.String())
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/new", hostFile)
   http.ListenAndServe(":1111", nil)
 }
 
-type Params struct {
-  File string
-}
-
 // Given a reader, extract a string and parse it into a struct
-func GetParams(req *http.Request) Params {
+func GetBody(req *http.Request) []byte {
   buf := new(bytes.Buffer)
   buf.ReadFrom(req.Body)
-
-  var i Params
-  json.Unmarshal(buf.Bytes(), &i)
-  return i
+  return buf.Bytes()
 }
