456b235cd097546b6025adb17b458c6ac1d3c5c9
Jeff Escalante <hello@jenius.me>
2014-06-27 14:29:26 -0400

    server now parses POST body

--- main.go
+++ main.go
@@ -5,13 +5,32 @@ package main
 import (
   "fmt"
   "net/http"
+  "bytes"
+  "io"
+  "encoding/json"
 )
 
 func handler(res http.ResponseWriter, req *http.Request) {
+  params := GetParams(req.Body)
+  fmt.Println(params.File)
   fmt.Fprint(res, "hello world!")
 }
 
 func main() {
 	http.HandleFunc("/", handler)
   http.ListenAndServe(":1111", nil)
+}
+
+type Params struct {
+  File string
+}
+
+// Given a reader, extract a string and parse it into a struct
+func GetParams(body io.Reader) Params {
+  buf := new(bytes.Buffer)
+  buf.ReadFrom(body)
+
+  var i Params
+  json.Unmarshal(buf.Bytes(), &i)
+  return i
 }
