b0c7c0809366e9ceac8a8e726ba2d3c9e29510de
Jeff Escalante <hello@jenius.me>
2014-06-30 10:47:36 -0400

    format

--- burner_api_test.go
+++ burner_api_test.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-  "testing"
-  "fmt"
-  "strings"
-  "net/http"
+	"fmt"
+	"net/http"
+	"strings"
+	"testing"
 )
 
 func TestBasicLogic(t *testing.T) {
-  req, err := http.NewRequest("POST", "http://example.com", strings.NewReader("wow"))
-  if err != nil {
-    t.Fail()
-  }
-  fmt.Println(req)
+	req, err := http.NewRequest("POST", "http://example.com", strings.NewReader("wow"))
+	if err != nil {
+		t.Fail()
+	}
+	fmt.Println(req)
 }
--- main.go
+++ main.go
@@ -3,71 +3,68 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "bytes"
-  "os"
-  "log"
-  "path/filepath"
-  "github.com/nu7hatch/gouuid"
+	"bytes"
+	"fmt"
+	"github.com/nu7hatch/gouuid"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
-/*
-- if not a POST, return a 404
-- Gets the contents of a POSTed file
-- Creates a UUID associated with it
-- Downloads the file locally, named UUID.tar.gz
-- Responds with the UUID
-*/
-
+// - if not a POST, return a 404
+// - Gets the contents of a POSTed file
+// - Creates a UUID associated with it
+// - Downloads the file locally, named UUID.tar.gz
+// - Responds with the UUID
 func hostFile(res http.ResponseWriter, req *http.Request) {
-  if req.Method != "POST" {
-    res.WriteHeader(404)
-    fmt.Fprint(res, "not found")
-    return
-  }
+	if req.Method != "POST" {
+		res.WriteHeader(404)
+		fmt.Fprint(res, "not found")
+		return
+	}
 
-  contents := GetBody(req)
+	contents := GetBody(req)
 
-  id, err := uuid.NewV4()
-  if err != nil {
-    log.Fatal(err)
-  }
+	id, err := uuid.NewV4()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  file, err := CreateFile(id.String())
-  if err != nil {
-    log.Fatal(err)
-  }
+	file, err := CreateFile(id.String())
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  file.Write(contents)
+	file.Write(contents)
 
-  fmt.Fprint(res, id.String())
+	fmt.Fprint(res, id.String())
 }
 
 func main() {
 	http.HandleFunc("/new", hostFile)
-  http.ListenAndServe(":1111", nil)
+	http.ListenAndServe(":1111", nil)
 }
 
 // Given a reader, extract a string and parse it into a struct
 func GetBody(req *http.Request) []byte {
-  buf := new(bytes.Buffer)
-  buf.ReadFrom(req.Body)
-  return buf.Bytes()
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(req.Body)
+	return buf.Bytes()
 }
 
 // given a file name, create ./files/NAME.tar.gz
 func CreateFile(id string) (*os.File, error) {
-  dirname, err := filepath.Abs(filepath.Dir(os.Args[0]))
-  if err != nil {
-    return nil, err
-  }
+	dirname, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		return nil, err
+	}
 
-  fPath := filepath.Join(dirname, "files", id + ".tar.gz")
-  file, err := os.Create(fPath)
-  if err != nil {
-    return nil, err
-  }
+	fPath := filepath.Join(dirname, "files", id+".tar.gz")
+	file, err := os.Create(fPath)
+	if err != nil {
+		return nil, err
+	}
 
-  return file, nil
+	return file, nil
 }
